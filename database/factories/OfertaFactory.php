5bf23e695a92c62e9c5a2b23c8c0370232941d39
rubengalan97 <ruben.galan.alu@iescampanillas.com>
2022-05-11 23:23:21 +0200

    Creacion de la estructura base, configuracion de la conexion a la base de datos, creacion de migraciones, factorias, seeder y modelos.

---
+++ database/factories/OfertaFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OfertaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
